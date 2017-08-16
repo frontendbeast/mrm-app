bdb4fc51321a11a4324d938b96774144e85dd839
Darren Hall <darren@darrenhall.co>
2017-08-16 11:13:08 +0100

    chore: initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+         <Text>Hello, its me</Text>
+      </View>
+    );
+  }
+}
