f29c06b784fd23e247f3f7063a35bffc5a661b96
Darren Hall <darren@darrenhall.co>
2017-11-29 17:05:12 +0000

    feat: add fabric

--- ios/MRMApp/AppDelegate.m
+++ ios/MRMApp/AppDelegate.m
@@ -9,6 +9,9 @@
 
 #import "AppDelegate.h"
 
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
@@ -28,6 +31,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [Fabric with:@[[Crashlytics class]]];
   return YES;
 }
 
